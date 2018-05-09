e25723786da3d9bb8c66e1b1b94c8db6e0a0e8da
TK <leandrotk100@gmail.com>
2018-05-09 00:09:42 -0300

    Return only one element on render

---
+++ 3.return_only_one_element.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class MyFirstComponent extends React.Component {
+  render() {
+    return (
+      <React.Fragment>
+        <h1>My title!</h1>
+        <p>Nice!</p>
+      </React.Fragment>
+    )
+  }
+}
