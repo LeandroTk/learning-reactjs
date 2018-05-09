a63b3e4048586788d72a40496def7106c02d7770
TK <leandrotk100@gmail.com>
2018-05-08 23:57:43 -0300

    Using className instead class on elements/components

---
+++ 2.class_name.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+class MyFirstComponent extends React.Component {
+  render() {
+    return <form className="myformzinho"></form>
+  }
+}
+
+export default MyFirstComponent;
