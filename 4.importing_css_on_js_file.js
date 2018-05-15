b1006f2fa8cc2256dc194daed9b3b0ceb3915748
TK <leandrotk100@gmail.com>
2018-05-14 23:36:14 -0300

    Importing css on a js file with webpack

---
+++ 4.importing_css_on_js_file.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './css/style.css';
+
+class MyComponentWithCss extends React.Component {
+  render() {
+    return (
+      <h1 class='my-title'>Title</h1>
+    )
+  }
+}
